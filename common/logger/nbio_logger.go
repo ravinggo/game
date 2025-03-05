243f03f7762d2d4807f8e1b9b80149e69e2e32d6
ravinggo <fj821431199@outlook.com>
2025-03-05 10:51:46 +0800

    add nbio_logger

--- common/logger/nbio_logger.go
+++ common/logger/nbio_logger.go
@@ -7,15 +7,15 @@ func NewBNLogger() NBLogger {
 	return NBLogger{}
 }
 
-func (this_ *NBLogger) Debug(format string, v ...interface{}) {
+func (this_ NBLogger) Debug(format string, v ...interface{}) {
 	Log.Debug().Msgf(format, v...)
 }
-func (this_ *NBLogger) Info(format string, v ...interface{}) {
+func (this_ NBLogger) Info(format string, v ...interface{}) {
 	Log.Info().Msgf(format, v...)
 }
-func (this_ *NBLogger) Warn(format string, v ...interface{}) {
+func (this_ NBLogger) Warn(format string, v ...interface{}) {
 	Log.Warn().Msgf(format, v...)
 }
-func (this_ *NBLogger) Error(format string, v ...interface{}) {
+func (this_ NBLogger) Error(format string, v ...interface{}) {
 	Log.Error().Msgf(format, v...)
 }
