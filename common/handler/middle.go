89407bfb289d02da730cb18835b522ca467b0b6b
ravinggo <fj821431199@outlook.com>
2025-06-03 10:23:17 +0800

    fix logger middle

--- common/handler/middle.go
+++ common/handler/middle.go
@@ -6,12 +6,10 @@ import (
 
 	"github.com/ravinggo/game/common/berror"
 	"github.com/ravinggo/game/common/ctx"
-	"github.com/ravinggo/game/common/define"
 )
 
 func LoggerAndRecover[TraceData any, TP ctx.TracePtr[TraceData]](next HandleFunc[TraceData, TP]) HandleFunc[TraceData, TP] {
 	return func(c *ctx.BaseCtx[TraceData, TP]) (err *berror.ErrMsg) {
-		msgName := string(define.ProtoMessageName(c.Req))
 		start := time.Now()
 		defer func() {
 			if e := recover(); e != nil {
@@ -21,7 +19,7 @@ func LoggerAndRecover[TraceData any, TP ctx.TracePtr[TraceData]](next HandleFunc
 				e := c.Info().Dur("duration", time.Since(start))
 				if len(c.Resp) > 0 {
 					for i := range c.Resp {
-						e = e.Str("resp", string(define.ProtoMessageName(c.Resp[i]))).Any("respData", c.Resp[i])
+						e = e.Any("respData", c.Resp[i])
 					}
 				}
 				e.Msg("success")
@@ -30,7 +28,7 @@ func LoggerAndRecover[TraceData any, TP ctx.TracePtr[TraceData]](next HandleFunc
 			}
 		}()
 
-		c.Info().Str("req", msgName).Any("reqData", c.Req).Msg("start")
+		c.Info().Any("reqData", c.Req).Msg("start")
 		err = next(c)
 		return
 	}
